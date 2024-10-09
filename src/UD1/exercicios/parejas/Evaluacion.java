6937e4800a1682a82fc2e453913244269a91f0ae
Pablo López <lopezpablocdm@gmail.com>
2024-10-09 14:23:31 +0200

    Ejercicio 4 parejas terminar

---
+++ src/UD1/exercicios/parejas/Evaluacion.java
@@ -0,0 +1,18 @@
+package UD1.exercicios.parejas;
+
+import java.util.Scanner;
+
+/** 
+ * Pablo López Couso - Piloto 
+ * Antonio Besada Ramilo - Copiloto
+*/
+
+public class Evaluacion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int pregunta1, pregunta2, pregunta3, pregunta4, pregunta5, pregunta6, ptsP1, ptsP2, ptsP3, ptsP4, ptsP5, ptsP6;
+
+        System.out.println("1. El programa funciona? Escoje: Si y funciona (1), Si pero no hace lo que debe (2), No (3)");
+        pregunta1 = sc.nextInt();
+    }
+}
