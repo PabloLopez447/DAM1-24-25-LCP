66f68821fcc847f5ece715469f547026ebcc0c8d
Pablo López <lopezpablocdm@gmail.com>
2024-10-10 13:02:44 +0200

    Ejercicio 129 Acabado

--- src/UD1/exercicios/lcp20241010/EP0129.java
+++ src/UD1/exercicios/lcp20241010/EP0129.java
@@ -4,12 +4,16 @@ import java.util.Scanner;
 
 public class EP0129 {
     public static void main(String[] args) {
-        int nIntroducido, cSumar;
+        int nIntroducido, cSumar, resto, multiplo;
         Scanner sc = new Scanner(System.in);
         System.out.print("Introduce el número que desees convertir en múltiplo de 7: ");
         nIntroducido = sc.nextInt();
         sc.close();
 
-        
+        resto = nIntroducido % 7;
+        cSumar = nIntroducido / 7 == 0 ? 0 : 7 - resto ;
+        multiplo = cSumar + nIntroducido;
+
+        System.out.println("El número a sumar es " + cSumar + " -> (" + cSumar + " + " + nIntroducido + " = " + multiplo + ")");
     }
 }
