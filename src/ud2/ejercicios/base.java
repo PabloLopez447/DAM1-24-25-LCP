52680483f9552dd032a1462a971a64ff435d9072
Pablo López Couso <lopezpablocdm@gmail.com>
2024-11-19 17:11:04 +0100

    Examen ready

---
+++ src/ud2/ejercicios/base.java
@@ -0,0 +1,18 @@
+package ud2.ejercicios;
+
+import java.util.Scanner;
+
+public class base {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("¿Cuáles son tus iniciales?");
+        String iniciales = scanner.nextLine();
+        System.out.println("¡Hola Mundo! Mi nombre es " + iniciales);
+
+        scanner.close();
+    }
+    
+}
