93e2f0bbe66f54f950c191b5bef6ba2a7225d587
Pablo Lopez <lopezpablocdm@gmail.com>
2024-11-27 13:18:11 +0100

    POO

--- src/ud2/lcpexamen/ChiclesRegalo.java
+++ src/ud2/lcpexamen/ChiclesRegalo.java
@@ -1,17 +1,40 @@
 package ud2.lcpexamen;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
 //Pablo López Couso
 
 public class ChiclesRegalo {
     public static void main(String[] args) {
-        
+
     }
 
     public static int totalChicles(int chiclesIni, int oferta, int ChiclesRegalo) {
         int totChicles = 0;
 
-        
-
         return totChicles;
-    }   
+    }
+
+    @Test
+    public void ChiclesRegaloTest() {
+        assertEquals(31, totalChicles(25, 5, 1));
+        assertEquals(6, totalChicles(5, 5, 1));
+        assertEquals(111, totalChicles(100, 10, 1));
+        assertEquals(-1, totalChicles(20, 2, 5));
+        assertEquals(-1, totalChicles(10, -1, 0));
+        assertEquals(-1, totalChicles(-1, 2, 1));
+        assertEquals(-1, totalChicles(10, 1, -1));
+        assertEquals(-1, totalChicles(20, 2, 2));
+        assertEquals(10, totalChicles(7, 3, 1));
+        assertEquals(40, totalChicles(27, 3, 1));
+        assertEquals(21, totalChicles(11, 2, 1));
+        assertEquals(31, totalChicles(16, 2, 1));
+        assertEquals(100, totalChicles(100, 0, 0));
+        assertEquals(100, totalChicles(100, 0, 1));
+        assertEquals(0, totalChicles(0, 0, 1));
+        assertEquals(0, totalChicles(0, 2, 1));
+    }
+
 }
